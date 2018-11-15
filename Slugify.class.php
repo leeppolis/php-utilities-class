328b4ee80525c95fa2e47ce24cac378b2237b44d
Simone Lippolis <leeppolis@gmail.com>
2018-11-15 11:26:49 +0100

    Update Slugify.class.php
    Added support for ICU \Transliterator Class

--- Slugify.class.php
+++ Slugify.class.php
@@ -9,37 +9,41 @@
  * @package SL
  * @subpackage Utilities
  */
-	namespace SL\Utilities;
+  namespace SL\Utilities;
 
 /**
  * @method string createSlug( string $text )
  */
  	
-	Class Slugify {		
-	
+Class Slugify {		
+
 /**
- * Given a string, returns the corresponding valid string that can be used as a slug in web pages' URL
- *
- * @param string $text
- * @return string 
- *
- */	
-			
-		public static function createSlug($text) { 
-			// replace non letter or digits by -
-			$text = preg_replace('~[^\\pL\d]+~u', '-', $text);			
-			// trim
-			$text = trim($text, '-');			
-			// transliterate
-			$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);			
-			// lowercase
-			$text = strtolower($text);			
-			// remove unwanted characters
-			$text = preg_replace('~[^-\w]+~', '', $text);			
-			if (empty($text)) {
-				return false;
-			}
-			return $text;
-		}
-	}
-?>
+* Given a string, returns the corresponding valid string that can be used as a slug in web pages' URL
+*
+* @param string $text
+* @return string 
+*
+*/	
+
+  public static function createSlug($text) { 
+    if (empty($text)) {
+      return false;
+    }
+    if ( class_exists('\Transliterator') ) {
+      $transliterator = \Transliterator::createFromRules(':: Any-Latin; :: Latin-ASCII; :: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;', \Transliterator::FORWARD);
+      return $transliterator->transliterate($text);
+    } else {			
+      // replace non letter or digits by -
+      $text = preg_replace('~[^\\pL\d]+~u', '-', $text);			
+      // trim
+      $text = trim($text, '-');			
+      // transliterate
+      $text = iconv('utf-8', 'ISO-8859-1//TRANSLIT', $text);			
+      // lowercase
+      $text = strtolower($text);			
+      // remove unwanted characters
+      $text = preg_replace('~[^-\w]+~', '', $text);
+      return $text;
+    }
+  }
+}
