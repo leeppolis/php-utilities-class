3a7e623612f7f0c18f7d5f160232d8f073f5877b
Simone Lippolis <leeppolis@gmail.com>
2018-11-15 11:33:10 +0100

    Update Slugify.class.php

--- Slugify.class.php
+++ Slugify.class.php
@@ -9,41 +9,40 @@
  * @package SL
  * @subpackage Utilities
  */
-  namespace SL\Utilities;
+	namespace SL\Utilities;
 
 /**
  * @method string createSlug( string $text )
  */
  	
-Class Slugify {		
-
+	Class Slugify {		
+	
 /**
-* Given a string, returns the corresponding valid string that can be used as a slug in web pages' URL
-*
-* @param string $text
-* @return string 
-*
-*/	
-
-  public static function createSlug($text) { 
-    if (empty($text)) {
-      return false;
-    }
-    if ( class_exists('\Transliterator') ) {
-      $transliterator = \Transliterator::createFromRules(':: Any-Latin; :: Latin-ASCII; :: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;', \Transliterator::FORWARD);
-      return $transliterator->transliterate($text);
-    } else {			
-      // replace non letter or digits by -
-      $text = preg_replace('~[^\\pL\d]+~u', '-', $text);			
-      // trim
-      $text = trim($text, '-');			
-      // transliterate
-      $text = iconv('utf-8', 'ISO-8859-1//TRANSLIT', $text);			
-      // lowercase
-      $text = strtolower($text);			
-      // remove unwanted characters
-      $text = preg_replace('~[^-\w]+~', '', $text);
+ * Given a string, returns the corresponding valid string that can be used as a slug in web pages' URL
+ *
+ * @param string $text
+ * @return string 
+ *
+ */	
+			
+		public static function createSlug($text) { 
+			if (empty($text)) {
+				return false;
+			}
+			if ( class_exists('\Transliterator') ) {
+  			$transliterator = \Transliterator::createFromRules(':: Any-Latin; :: Latin-ASCII; :: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;', \Transliterator::FORWARD);
+  			$text = $transliterator->transliterate($text);	
+			}			
+			// replace non letter or digits by -
+			$text = preg_replace('~[^\\pL\d]+~u', '-', $text);			
+			// trim
+			$text = trim($text, '-');			
+			// transliterate
+			$text = iconv('utf-8', 'ISO-8859-1//TRANSLIT', $text);			
+			// lowercase
+			$text = strtolower($text);			
+			// remove unwanted characters
+			$text = preg_replace('~[^-\w]+~', '', $text);
       return $text;
-    }
-  }
-}
+		}
+	}
